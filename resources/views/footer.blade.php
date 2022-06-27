0e03195f34d5d27a6da5ae445d3daccc6fce1003
Yutaro.Sugaya <tech_nine0400@yahoo.co.jp>
2022-06-28 08:07:24 +0900

    commit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="container text-center">
-    <span class="text-light"></span>
+    <span class="text-light">testooooo</span>
 </div>
--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('ver','API\VerController@index');
+
 // 以下を追加！
 Route::get('test', 'TestAPIController@index');
+
+Route::middleware(['middleware' => 'api'])->group(function () {
+    # 投稿作成
+    Route::post('/posts/create', 'PostController@create');
+    # 投稿一覧表示
+    Route::get('/posts', 'PostController@index');
+    # 投稿表示
+    Route::get('/posts/{id}', 'PostController@show');
+    # 投稿編集
+    Route::patch('/posts/update/{id}' , 'PostController@update');
+    # 投稿削除
+    Route::delete('/posts/{id}', 'PostController@delete');
+});
